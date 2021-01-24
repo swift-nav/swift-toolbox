f4b860f24b3f414e467a0eeb24cbfc9cf4d3ada1
Jason Mobarak <jason@swiftnav.com>
2021-01-23 18:37:34 -0800

    add basic Rust python module

---
+++ console_backend/console_backend/__init__.py
@@ -0,0 +1,3 @@
+"""
+Top level module for the Console backend.
+"""
---
+++ console_backend/src/server.rs
@@ -0,0 +1,27 @@
+//! Objects related to PyBuffer and PyStr
+use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+
+/// The backend server
+#[pyclass]
+struct Server {}
+
+#[pymethods]
+impl Server {
+    #[new]
+    pub fn __new__() -> Self {
+        Server {}
+    }
+
+    #[text_signature = "($self, bytes, /)"]
+    pub fn from_bytes(&mut self, bytes: &PyBytes) -> PyResult<usize> {
+        let byte_vec: Vec<u8> = bytes.extract().unwrap();
+        Ok(byte_vec.len())
+    }
+}
+
+#[pymodule]
+pub fn server(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Server>()?;
+    Ok(())
+}

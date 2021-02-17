90d2e89df49b6d68a0c2e005c897308d61930e3f
John-Michael Burke <43353147+john-michaelburke@users.noreply.github.com>
2021-02-16 17:02:20 -0800

    [CPP-56][CPP-57]Windows CD and add negligible filter for Windows/Linux CD. (#6)
    * [CPP-57]Windows CD
    
    * Add mechanism to delete unwanted Qt things for fbs.
    
    * Respond to review request, use cargo makes platform override for prod-installer task.
    
    * Respond to review requests.

--- .github/ci-build.sh
+++ .github/ci-build.sh
@@ -8,7 +8,6 @@ set -o pipefail
 archive_name="console_pp"
 
 if [[ "$OS_NAME" == "macOS" ]]; then
-    (strip 'Swift Navigation Console.dmg');
     tar -czf ${archive_name}_osx.tar.gz 'Swift Navigation Console.dmg';
     VERSION="$(git describe --always --tags --dirty)";
     BUILD_TRIPLET="$(gcc -dumpmachine)";
@@ -18,11 +17,18 @@ if [[ "$OS_NAME" == "macOS" ]]; then
 fi
 
 if [[ "$OS_NAME" == "Linux" ]]; then
-    (cd target; strip 'Swift Navigation Console.deb');
     tar -C "target" -czf ${archive_name}_linux.tar.gz 'Swift Navigation Console.deb';
     VERSION="$(git describe --always --tags --dirty)";
     BUILD_TRIPLET="$(gcc -dumpmachine)";
     mv ${archive_name}_linux.tar.gz "${archive_name}-${VERSION}-${BUILD_TRIPLET}.tar.gz";
     echo "${archive_name}-${VERSION}-${BUILD_TRIPLET}.tar.gz" >release-archive.filename;
+    ls -l;
+fi
+
+if [[ "$OS_NAME" == "Windows" ]]; then
+    VERSION="$(git describe --always --tags)";
+    BUILD_TRIPLET="x86_64-pc-windows-msvc";
+    mv 'target/Swift Navigation ConsoleSetup.exe' "${archive_name}-${VERSION}-windows-${BUILD_TRIPLET}.exe";
+    echo "${archive_name}-${VERSION}-windows-${BUILD_TRIPLET}.exe" >release-archive.filename;
     ls -l;
 fi
---
+++ utils/get_fbs_remove_list.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+FBS_DIR = "src/main/fbs"
+FILENAME_POSTFIX = "-remove-negligibles.sh"
+INSTALLER_DIR = "target/Swift Navigation Console"
+
+BLACKLIST = ["translations", "QtQuick3D", "Web"]
+SYS_PLATFORM_PREFIXS = {"win32": "windows", "linux": "linux"}
+
+
+def create_remove_negligibles_file():
+    """Create a platform specific bash script to remove undesirables before fbs
+    installer command operation.
+    """
+    out = ""
+    for root, _, files in os.walk(f"{INSTALLER_DIR}", topdown=False):
+        for name in files:
+            path = os.path.relpath(os.path.join(root, name), f"{INSTALLER_DIR}")
+            if not any([x in path for x in BLACKLIST]):
+                continue
+            out += f'rm -rf "{INSTALLER_DIR}/{path}" && '
+    filename_prefix = SYS_PLATFORM_PREFIXS.get(sys.platform, "unknown")
+    with open(os.path.join(FBS_DIR, filename_prefix + FILENAME_POSTFIX), "w") as filo:
+        filo.write(out[: -len("&& ")])
+        filo.write("\n")
+
+
+if __name__ == "__main__":
+    create_remove_negligibles_file()

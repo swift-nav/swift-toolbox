6f593eb5bbeac1b2502218e8f4e8798d2027d947
john-michaelburke <johnmichael.burke@swift-nav.com>
2021-02-26 16:42:22 -0800

    Respond to review requests.

--- utils/disk_usage_benchmarks.py
+++ utils/disk_usage_benchmarks.py
@@ -11,38 +11,19 @@ ERROR_MARGIN_FRAC = "error_margin_frac"
 
 DISK_USAGE_COMMAND = lambda file_path: f"du -ch {file_path} | grep total"
 
-RUST_BENCHMARKS = {
+INSTALLER_BENCHMARKS = {
     WINDOWS: [
-        {
-            NAME: "Windows Installer",
-            FILE_PATH: "release-archive.filename",
-            EXPECTED: 55,
-            ERROR_MARGIN_FRAC: 0.05,
-        },
+        {NAME: "Windows Installer", FILE_PATH: "release-archive.filename", EXPECTED: 55, ERROR_MARGIN_FRAC: 0.05,},
     ],
-    MACOS: [
-        {
-            NAME: "macOS Installer",
-            FILE_PATH: "release-archive.filename",
-            EXPECTED: 95,
-            ERROR_MARGIN_FRAC: 0.05,
-        },
-    ],
-    LINUX: [
-        {
-            NAME: "Linux Installer",
-            FILE_PATH: "release-archive.filename",
-            EXPECTED: 85,
-            ERROR_MARGIN_FRAC: 0.05,
-        },
-    ],
+    MACOS: [{NAME: "macOS Installer", FILE_PATH: "release-archive.filename", EXPECTED: 95, ERROR_MARGIN_FRAC: 0.05,},],
+    LINUX: [{NAME: "Linux Installer", FILE_PATH: "release-archive.filename", EXPECTED: 85, ERROR_MARGIN_FRAC: 0.05,},],
 }
 
 
 def run_disk_usage_benchmark():
     """Runner for disk usage benchmark validations.
     """
-    for os_, benchmarks in RUST_BENCHMARKS.items():
+    for os_, benchmarks in INSTALLER_BENCHMARKS.items():
         if os_ != sys.platform:
             continue
         for bench in benchmarks:
@@ -51,17 +32,13 @@ def run_disk_usage_benchmark():
                 release_file = archive_file.readline().rstrip()
 
             bench_command = f"{DISK_USAGE_COMMAND(release_file)}"
-            disk_usage = subprocess.run(
-                bench_command, shell=True, text=True, check=True, capture_output=True
-            )
+            disk_usage = subprocess.run(bench_command, shell=True, text=True, check=True, capture_output=True)
             print(disk_usage)
             disk_usage, _ = disk_usage.stdout.split("\t")
             print(disk_usage)
 
             disk_usage = float(disk_usage.rstrip("M"))
-            assert (
-                disk_usage is not None
-            ), f"Test:{bench[NAME]} retrieved bench value None."
+            assert disk_usage is not None, f"Test:{bench[NAME]} retrieved bench value None."
             assert disk_usage >= bench[ERROR_MARGIN_FRAC] * bench[EXPECTED], (
                 f"Test:{bench[NAME]} Bench Value:{disk_usage} not larger than "
                 f"{bench[ERROR_MARGIN_FRAC]*bench[EXPECTED]}MB."

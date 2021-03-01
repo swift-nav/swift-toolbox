c45d87dcaecfb5f8161f71aff497c374700766ee
john-michaelburke <johnmichael.burke@swift-nav.com>
2021-03-01 13:40:19 -0800

    Respond to review requests.

--- utils/disk_usage_benchmarks.py
+++ utils/disk_usage_benchmarks.py
@@ -23,31 +23,30 @@ INSTALLER_BENCHMARKS = {
 def run_disk_usage_benchmark():
     """Runner for disk usage benchmark validations.
     """
-    for os_, benchmarks in INSTALLER_BENCHMARKS.items():
-        if os_ != sys.platform:
-            continue
-        for bench in benchmarks:
-            release_file = ""
-            with open(bench[FILE_PATH]) as archive_file:
-                release_file = archive_file.readline().rstrip()
+    os_ = sys.platform
+    benchmarks = INSTALLER_BENCHMARKS.get(os_, [])
+    for bench in benchmarks:
+        release_file = ""
+        with open(bench[FILE_PATH]) as archive_file:
+            release_file = archive_file.readline().rstrip()
 
-            bench_command = f"{DISK_USAGE_COMMAND(release_file)}"
-            disk_usage = subprocess.run(bench_command, shell=True, text=True, check=True, capture_output=True)
-            print(disk_usage)
-            disk_usage, _ = disk_usage.stdout.split("\t")
-            print(disk_usage)
+        bench_command = DISK_USAGE_COMMAND(release_file)
+        disk_usage = subprocess.run(bench_command, shell=True, text=True, check=True, capture_output=True)
+        print(disk_usage)
+        disk_usage, _ = disk_usage.stdout.split("\t")
+        print(disk_usage)
 
-            disk_usage = float(disk_usage.rstrip("M"))
-            assert disk_usage is not None, f"Test:{bench[NAME]} retrieved bench value None."
-            assert disk_usage >= bench[ERROR_MARGIN_FRAC] * bench[EXPECTED], (
-                f"Test:{bench[NAME]} Bench Value:{disk_usage} not larger than "
-                f"{bench[ERROR_MARGIN_FRAC]*bench[EXPECTED]}MB."
-            )
-            assert disk_usage - bench[EXPECTED] <= bench[ERROR_MARGIN_FRAC] * bench[EXPECTED], (  # type: ignore
-                f"Test:{bench[NAME]} Bench Value:{disk_usage} not within "
-                f"{bench[ERROR_MARGIN_FRAC]} of {bench[EXPECTED]}."
-            )
-            print(f"PASS - {os_}:{bench[NAME]} MARGIN={disk_usage - bench[EXPECTED]}")
+        disk_usage = float(disk_usage.rstrip("M"))
+        assert disk_usage is not None, f"Test:{bench[NAME]} retrieved bench value None."
+        assert disk_usage >= bench[ERROR_MARGIN_FRAC] * bench[EXPECTED], (
+            f"Test:{bench[NAME]} Bench Value:{disk_usage} not larger than "
+            f"{bench[ERROR_MARGIN_FRAC]*bench[EXPECTED]}MB."
+        )
+        assert disk_usage - bench[EXPECTED] <= bench[ERROR_MARGIN_FRAC] * bench[EXPECTED], (  # type: ignore
+            f"Test:{bench[NAME]} Bench Value:{disk_usage} not within "
+            f"{bench[ERROR_MARGIN_FRAC]} of {bench[EXPECTED]}."
+        )
+        print(f"PASS - {os_}:{bench[NAME]} MARGIN={disk_usage - bench[EXPECTED]}")
 
 
 if __name__ == "__main__":

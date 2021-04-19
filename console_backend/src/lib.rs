5e8f0fe60d04132727639495926a212c21847ebf
John Vandenberg <jayvdb@gmail.com>
2021-04-20 07:04:36 +0800

    Extract codes from piksi_tools

--- console_backend/src/lib.rs
+++ console_backend/src/lib.rs
@@ -2,6 +2,7 @@ pub mod console_backend_capnp {
     include!(concat!(env!("OUT_DIR"), "/console_backend_capnp.rs"));
 }
 pub mod common_constants;
+pub mod piksi_tools_constants;
 pub mod constants;
 pub mod date_conv;
 pub mod formatters;

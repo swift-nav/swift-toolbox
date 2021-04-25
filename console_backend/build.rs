324c7feeebbace3cd8117fd99c829b2008533762
John Vandenberg <jayvdb@gmail.com>
2021-04-25 10:34:40 +0800

    build.rs: Rebuild if capnp changes

--- console_backend/build.rs
+++ console_backend/build.rs
@@ -3,6 +3,7 @@ extern crate capnpc;
 fn main() {
     let output_dir = std::env::var("OUT_DIR").unwrap();
 
+    println!("cargo:rerun-if-changed=console_backend.capnp");
     ::capnpc::CompilerCommand::new()
         .file("console_backend.capnp")
         .output_path(output_dir)

b66b2e67c3af89a06f9909f1c404514891b8924d
John Vandenberg <jayvdb@gmail.com>
2021-05-10 22:01:41 +0800

    Include more of piksi-tools utils.py

--- utils/extract_piksi_tools_constants.py
+++ utils/extract_piksi_tools_constants.py
@@ -9,35 +9,27 @@ def main():
     resume = "CODE_GPS_L1CA"
     resume_after = None
     for line in lines:
-        if resume and line.startswith(resume):
+        if line.startswith("def ") and line.endswith("(code):"):
+            # help the py2many type inference system
+            # https://github.com/adsharma/py2many/issues/246
+            line = line.replace("(code):", "(code: int):")
+
+        if resume and line.strip().startswith(resume):
             emit = True
             resume_after = resume = None
         elif resume_after and line.startswith(resume_after):
             emit = True
             resume_after = resume = None
             continue
-        # These three codes have been given improved strings
-        # https://snav.slack.com/archives/C02USAD03/p1618522328058800
-        elif line.startswith("GAL_E8I_STR = "):
-            print('GAL_E8I_STR = "GAL E5ab I"')
-            continue
-        elif line.startswith("GAL_E8Q_STR = "):
-            print('GAL_E8Q_STR = "GAL E5ab Q"')
-            continue
-        elif line.startswith("GAL_E8X_STR = "):
-            print('GAL_E8X_STR = "GAL E5ab I+Q"')
-            continue
-        elif line.startswith("GUI_CODES = {"):
-            emit = False
-            resume = "GPS_L1CA_STR"
         elif line.startswith("color_dict"):
+            # Depends on https://github.com/adsharma/py2many/issues/171
             emit = False
-            resume = "gps_codes ="
-        elif line.startswith("def code_is_"):
-            emit = False
-            resume_after = "    return code in "
+            resume = "def code_to_str"
         elif line.startswith("def get_mode"):
-            break
+            # get_mode uses SBP codes from an import, and the `msg` class.
+            # The functions which follow `get_mode` utilise `datetime` or
+            # use str.format https://github.com/adsharma/py2many/issues/73
+            emit = False
 
         if emit:
             print(line.rstrip())

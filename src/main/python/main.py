1389ef83903a512ba515bb0f30ae134b3f09bc35
John Michael Burke <johnmichael.burke@swift-nav.com>
2021-05-12 20:46:52 -0700

    Moved navbar and log panel out of main.py.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,7 +5,7 @@ import os
 import sys
 import threading
 
-from typing import Dict, List, Any
+from typing import List, Any
 
 import capnp  # type: ignore
 
@@ -13,14 +13,28 @@ from PySide2.QtWidgets import QApplication  # type: ignore
 
 from fbs_runtime.application_context.PySide2 import ApplicationContext  # type: ignore  # pylint: disable=unused-import
 
-from PySide2.QtCore import Property, QMutex, QObject, QUrl, QPointF, Slot
+from PySide2.QtCore import QObject, QUrl, QPointF, Slot
 from PySide2.QtCharts import QtCharts  # pylint: disable=unused-import
 
 from PySide2 import QtQml, QtCore
 
 from PySide2.QtQml import QQmlComponent, qmlRegisterType
 
-from constants import ApplicationStates, MessageKeys, Keys, QTKeys, Tabs
+from constants import ApplicationStates, MessageKeys, Keys, Tabs
+
+from log_panel import (
+    LOG_PANEL,
+    log_panel_lock,
+    LogPanelData,
+    LogPanelModel,
+)
+
+from nav_bar import (
+    NAV_BAR,
+    NavBarData,
+    NavBarModel,
+)
+
 from observation_tab import (
     ObservationData,
     ObservationModel,
@@ -101,21 +115,6 @@ TAB_LAYOUT = {
     },
 }
 
-LOG_PANEL: Dict[str, Any] = {
-    Keys.ENTRIES: [],
-}
-log_panel_lock = QMutex()
-
-NAV_BAR: Dict[str, Any] = {
-    Keys.AVAILABLE_PORTS: [],
-    Keys.AVAILABLE_BAUDRATES: [],
-    Keys.AVAILABLE_FLOWS: [],
-    Keys.AVAILABLE_REFRESH_RATES: [],
-    Keys.CONNECTED: False,
-    Keys.PREVIOUS_HOSTS: [],
-    Keys.PREVIOUS_PORTS: [],
-    Keys.PREVIOUS_FILES: [],
-}
 
 capnp.remove_import_hook()  # pylint: disable=no-member
 
@@ -309,135 +308,6 @@ class DataModel(QObject):
         m.solutionPositionStatusButtonFront.solutionPositionCenter = buttons[3]
         buffer = m.to_bytes()
         self.endpoint.send_message(buffer)
-
-
-class LogPanelData(QObject):
-    _entries: List[str] = []
-
-    def get_entries(self) -> List[str]:
-        """Getter for _entries."""
-        return self._entries
-
-    def set_entries(self, entries: List[str]) -> None:
-        """Setter for _entries."""
-        self._entries = entries
-
-    entries = Property(QTKeys.QVARIANTLIST, get_entries, set_entries)  # type: ignore
-
-    def append_entries(self, entries: List[str]) -> None:
-        self._entries += entries
-
-
-class LogPanelModel(QObject):  # pylint: disable=too-few-public-methods
-    @Slot(LogPanelData)  # type: ignore
-    def fill_data(self, cp: LogPanelData) -> LogPanelData:  # pylint:disable=no-self-use
-        # Avoid locking so that message processor has priority to lock
-        if LOG_PANEL[Keys.ENTRIES]:
-            if log_panel_lock.try_lock():
-                cp.append_entries(LOG_PANEL[Keys.ENTRIES])
-                LOG_PANEL[Keys.ENTRIES][:] = []
-                log_panel_lock.unlock()
-        return cp
-
-
-class NavBarData(QObject):  # pylint: disable=too-many-instance-attributes
-
-    _available_ports: List[str] = []
-    _available_baudrates: List[str] = []
-    _available_flows: List[str] = []
-    _available_refresh_rates: List[str] = []
-    _connected: bool = False
-    _previous_hosts: List[str] = []
-    _previous_ports: List[str] = []
-    _previous_files: List[str] = []
-
-    def get_available_ports(self) -> List[str]:
-        return self._available_ports
-
-    def set_available_ports(self, available_ports: List[str]) -> None:
-        self._available_ports = available_ports
-
-    available_ports = Property(QTKeys.QVARIANTLIST, get_available_ports, set_available_ports)  # type: ignore
-
-    def get_available_baudrates(self) -> List[str]:
-        return self._available_baudrates
-
-    def set_available_baudrates(self, available_baudrates: List[str]) -> None:
-        self._available_baudrates = available_baudrates
-
-    available_baudrates = Property(
-        QTKeys.QVARIANTLIST, get_available_baudrates, set_available_baudrates  # type: ignore
-    )
-
-    def get_available_flows(self) -> List[str]:
-        return self._available_flows
-
-    def set_available_flows(self, available_flows: List[str]) -> None:
-        self._available_flows = available_flows
-
-    available_flows = Property(QTKeys.QVARIANTLIST, get_available_flows, set_available_flows)  # type: ignore
-
-    def get_available_refresh_rates(self) -> List[str]:
-        return self._available_refresh_rates
-
-    def set_available_refresh_rates(self, available_refresh_rates: List[str]) -> None:
-        self._available_refresh_rates = available_refresh_rates
-
-    available_refresh_rates = Property(
-        QTKeys.QVARIANTLIST, get_available_refresh_rates, set_available_refresh_rates  # type: ignore
-    )
-
-    def get_connected(self) -> bool:
-        """Getter for _connected.
-
-        Returns:
-            bool: Whether a connection is live or not.
-        """
-        return self._connected
-
-    def set_connected(self, connected: bool) -> None:
-        """Setter for _connected."""
-        self._connected = connected
-
-    connected = Property(bool, get_connected, set_connected)
-
-    def get_previous_hosts(self) -> List[str]:
-        return self._previous_hosts
-
-    def set_previous_hosts(self, previous_hosts: List[str]) -> None:
-        self._previous_hosts = previous_hosts
-
-    previous_hosts = Property(QTKeys.QVARIANTLIST, get_previous_hosts, set_previous_hosts)  # type: ignore
-
-    def get_previous_ports(self) -> List[str]:
-        return self._previous_ports
-
-    def set_previous_ports(self, previous_ports: List[str]) -> None:
-        self._previous_ports = previous_ports
-
-    previous_ports = Property(QTKeys.QVARIANTLIST, get_previous_ports, set_previous_ports)  # type: ignore
-
-    def get_previous_files(self) -> List[str]:
-        return self._previous_files
-
-    def set_previous_files(self, previous_files: List[str]) -> None:
-        self._previous_files = previous_files
-
-    previous_files = Property(QTKeys.QVARIANTLIST, get_previous_files, set_previous_files)  # type: ignore
-
-
-class NavBarModel(QObject):  # pylint: disable=too-few-public-methods
-    @Slot(NavBarData)  # type: ignore
-    def fill_data(self, cp: NavBarData) -> NavBarData:  # pylint:disable=no-self-use
-        cp.set_available_ports(NAV_BAR[Keys.AVAILABLE_PORTS])
-        cp.set_available_baudrates(NAV_BAR[Keys.AVAILABLE_BAUDRATES])
-        cp.set_available_flows(NAV_BAR[Keys.AVAILABLE_FLOWS])
-        cp.set_available_refresh_rates(NAV_BAR[Keys.AVAILABLE_REFRESH_RATES])
-        cp.set_connected(NAV_BAR[Keys.CONNECTED])
-        cp.set_previous_hosts(NAV_BAR[Keys.PREVIOUS_HOSTS])
-        cp.set_previous_ports(NAV_BAR[Keys.PREVIOUS_PORTS])
-        cp.set_previous_files(NAV_BAR[Keys.PREVIOUS_FILES])
-        return cp
 
 
 def is_frozen() -> bool:

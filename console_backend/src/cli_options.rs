99d1619004cc37bb60f8bd5e8e03cd3212cff2ad
Jason Mobarak <jason@swiftnav.com>
2021-05-17 16:37:21 -0700

    fix-up arg filtering for windows

--- console_backend/src/cli_options.rs
+++ console_backend/src/cli_options.rs
@@ -39,12 +39,13 @@ impl CliOptions {
         let mut filtered_args: Vec<String> = vec![];
         for arg in args {
             if let Some(n_arg) = next_args.next() {
-                if arg == "python" && n_arg.ends_with(".py") {
+                if (arg.ends_with("python") || arg.ends_with("python.exe")) && n_arg.ends_with(".py") {
                     continue;
                 }
             }
             filtered_args.push(arg);
         }
+        eprintln!("{:?}", filtered_args);
         CliOptions::parse_from(filtered_args)
     }
 }

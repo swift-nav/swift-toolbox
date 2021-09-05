be743c7609b4c88a42af2beaf9f8fc88f701caa2
Jason Mobarak <jason@swiftnav.com>
2021-09-04 18:00:20 -0700

    fixup

--- console_backend/src/settings_tab.rs
+++ console_backend/src/settings_tab.rs
@@ -708,7 +708,7 @@ mod client {
 
         fn release(&self) {
             // Safety: Only called via libsettings_unlock after libsettings_lock was called
-            unsafe { self.0.force_unlock() };
+            unsafe { self.0.force_unlock() }
         }
     }
 

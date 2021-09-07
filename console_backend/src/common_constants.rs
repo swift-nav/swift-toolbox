3054dc0547f481be8999d12b1bda33283346359d
Steve Meyer <steve.meyer@swift-nav.com>
2021-09-06 17:45:40 -0700

    generate common constants

--- console_backend/src/common_constants.rs
+++ console_backend/src/common_constants.rs
@@ -1,21 +1,6 @@
-//! ```cargo
-//! [package]
-//! edition = "2018"
-//! [dependencies]
-//! strum = "*"
-//! strum_macros = "*"
-//! ```
+// cargo-deps: strum,strum_macros
 
-#![allow(clippy::collapsible_else_if)]
-#![allow(clippy::double_parens)] // https://github.com/adsharma/py2many/issues/17
-#![allow(clippy::map_identity)]
-#![allow(clippy::needless_return)]
-#![allow(clippy::print_literal)]
-#![allow(clippy::ptr_arg)]
-#![allow(clippy::redundant_static_lifetimes)] // https://github.com/adsharma/py2many/issues/266
-#![allow(clippy::unnecessary_cast)]
 #![allow(clippy::upper_case_acronyms)]
-#![allow(clippy::useless_vec)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
@@ -213,6 +198,8 @@ pub enum Keys {
     XMIN,
     #[strum(serialize = "XMAX")]
     XMAX,
+    #[strum(serialize = "IMPORT_STATUS")]
+    IMPORT_STATUS,
 }
 
 #[derive(Clone, Debug, Display, EnumString, EnumVariantNames, Eq, Hash, PartialEq)]

1ae370bc6f17b0880fd8628cd66c0feb5cf6e82f
Jason Mobarak <jason@swiftnav.com>
2021-09-22 17:54:47 -0700

    enable jemallocator

--- console_backend/src/lib.rs
+++ console_backend/src/lib.rs
@@ -47,6 +47,13 @@ use crate::{
     tracking_signals_tab::TrackingSignalsTab, update_tab::UpdateTab,
 };
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 struct Tabs<'link, S: types::CapnProtoSender> {
     pub main: Mutex<MainTab<S>>,
     pub advanced_ins: Mutex<AdvancedInsTab<S>>,

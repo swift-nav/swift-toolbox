8c6c2cc51b66f2ee7b832c90afcc433b3d2ddb29
Steve Meyer <steve.meyer@swift-nav.com>
2021-10-03 09:48:53 -0700

    unix python download script

---
+++ standalone-py/download.sh
@@ -0,0 +1,19 @@
+base_url="https://github.com/indygreg/python-build-standalone/releases/download/20210724"
+
+case "$(uname -s)" in
+Darwin)
+    cpython_release="$base_url/cpython-3.9.6-x86_64-apple-darwin-lto-20210724T1424.tar.zst"
+    ;;
+Linux)
+    cpython_release="$base_url/cpython-3.9.6-x86_64-unknown-linux-gnu-lto-20210724T1424.tar.zst"
+    ;;
+*)
+    echo "unknown os"
+    exit 1
+    ;;
+esac
+
+wget $cpython_release -O py39.tar.zst
+
+zstd -d py39.tar.zst -o py39.tar
+tar -xvf py39.tar

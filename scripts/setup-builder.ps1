f3518df587f53436a5f760ed0b3c95886003f17f
Steve Meyer <steve.meyer@swift-nav.com>
2021-10-05 13:49:16 -0700

    remove scripts

--- scripts/setup-builder.ps1
+++
@@ -1,5 +0,0 @@
-Copy-Item -Recurse .\standalone-py\python\install py39
-.\py39\python ".\get-pip.py"
-.\py39\python -m pip install flit
-.\py39\python -m pip install .
-.\py39\python -m pip install ".[test]"
--- scripts/setup-builder.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-cp -r ./standalone-py/python/install py39
-./py39/bin/python3 ./get-pip.py
-./py39/bin/python3 -m pip install flit
-./py39/bin/python3 -m pip install .
-./py39/bin/python3 -m pip install ".[test]"
-

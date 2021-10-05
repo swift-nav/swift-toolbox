95b13a5d70e6e01fae9e4798766442ba18677b9e
Steve Meyer <steve.meyer@swift-nav.com>
2021-10-04 18:39:07 -0700

    powershell fix

--- scripts/setup-builder.ps1
+++ scripts/setup-builder.ps1
@@ -1,4 +1,5 @@
 Copy-Item -Recurse .\standalone-py\python\install py39
 .\py39\python ".\get-pip.py"
 .\py39\python -m pip install flit
-.\py39\python -m pip install -r ".[test]"
+.\py39\python -m pip install .
+.\py39\python -m pip install ".[test]"
--- scripts/setup-builder.sh
+++ scripts/setup-builder.sh
@@ -4,5 +4,6 @@ set -ex
 cp -r ./standalone-py/python/install py39
 ./py39/bin/python3 ./get-pip.py
 ./py39/bin/python3 -m pip install flit
+./py39/bin/python3 -m pip install .
 ./py39/bin/python3 -m pip install ".[test]"
 

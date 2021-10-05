2ca9bb28775541378e2023ad201b682a43ffb8d0
Steve Meyer <steve.meyer@swift-nav.com>
2021-10-05 13:08:56 -0700

    ci-build.sh updates

--- .github/ci-build.sh
+++ .github/ci-build.sh
@@ -5,52 +5,61 @@ set -x
 set -o errexit
 set -o pipefail
 
-archive_name="console_pp"
-DATE="$(date '+%Y-%m-%d')";
+ARCHIVE_NAME="console_pp"
+DATE="$(date '+%Y-%m-%d')"
+PYTHON_DIST = "py39-dist.tar.xz"
 
 if [[ "$OS_NAME" == "macOS" ]]; then
-    tar -czf ${archive_name}_osx.tar.gz 'swift_navigation_console.dmg';
-    VERSION="$(git describe --always --tags --dirty)";
-    BUILD_TRIPLET="$(gcc -dumpmachine)";
-    mv ${archive_name}_osx.tar.gz "${archive_name}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz";
-    echo "${archive_name}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz" >release-archive.filename;
+    VERSION="$(git describe --always --tags --dirty)"
+    BUILD_TRIPLET="$(gcc -dumpmachine)"
+    OUTPUT_NAME = "${ARCHIVE_NAME}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz"
 
-    cp -r console_backend/tests/data 'main.dist' && cp utils/bench_runner.py 'main.dist';
-    cd 'main.dist';
-    7z a -tzip "../$OS_NAME.zip" *;
-    cd ..;
-    echo "${OS_NAME}.zip" >bench.filename;
-    ls -l;
+    mv ${PYTHON_DIST} ${OUTPUT_NAME}
+    echo ${OUTPUT_NAME} >release-archive.filename
+
+    mkdir "main.dist"
+    cp -r console_backend/tests/data "main.dist"
+    cp utils/bench_runner.py "main.dist"
+    cd "main.dist"
+    7z a -tzip "../$OS_NAME.zip" *
+    cd ..
+    echo "${OS_NAME}.zip" >bench.filename
+
+    ls -l
 fi
 
 if [[ "$OS_NAME" == "Linux" ]]; then
-    tar -C "target" -czf ${archive_name}_linux.tar.gz 'swift_navigation_console.deb';
-    VERSION="$(git describe --always --tags --dirty)";
-    BUILD_TRIPLET="$(gcc -dumpmachine)";
-    mv ${archive_name}_linux.tar.gz "${archive_name}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz";
-    echo "${archive_name}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz" >release-archive.filename;
+    VERSION="$(git describe --always --tags --dirty)"
+    BUILD_TRIPLET="$(gcc -dumpmachine)"
+    OUTPUT_NAME = "${ARCHIVE_NAME}-${VERSION}-${BUILD_TRIPLET}-${DATE}.tar.gz"
 
-    cp -r console_backend/tests/data 'target/swift_navigation_console' && cp utils/bench_runner.py 'target/swift_navigation_console';
-    cd 'target/swift_navigation_console';
-    7z a -tzip "../../$OS_NAME.zip" *;
-    cd ../..;
-    echo "${OS_NAME}.zip" >bench.filename;
+    mv ${PYTHON_DIST} ${OUTPUT_NAME}
+    echo ${OUTPUT_NAME} >release-archive.filename
 
-    ls -l;
+    cp -r console_backend/tests/data "target/swift_navigation_console"
+    cp utils/bench_runner.py "target/swift_navigation_console"
+    cd "target/swift_navigation_console"
+    7z a -tzip "../../$OS_NAME.zip" *
+    cd ../..
+    echo "${OS_NAME}.zip" >bench.filename
+
+    ls -l
 fi
 
 if [[ "$OS_NAME" == "Windows" ]]; then
-    VERSION="$(git describe --always --tags)";
-    BUILD_TRIPLET="x86_64-pc-windows-msvc";
+    VERSION="$(git describe --always --tags)"
+    BUILD_TRIPLET="x86_64-pc-windows-msvc"
+    OUTPUT_NAME = "${ARCHIVE_NAME}-${VERSION}-windows-${BUILD_TRIPLET}-${DATE}.tar.gz"
 
-    mv 'target/swift_navigation_consoleSetup.exe' "${archive_name}-${VERSION}-windows-${BUILD_TRIPLET}-${DATE}.exe";
-    echo "${archive_name}-${VERSION}-windows-${BUILD_TRIPLET}-${DATE}.exe" >release-archive.filename;
+    mv ${PYTHON_DIST} ${OUTPUT_NAME}
+    echo ${OUTPUT_NAME} >release-archive.filename
 
-    cp -r console_backend/tests/data 'target/swift_navigation_console' && cp utils/bench_runner.py 'target/swift_navigation_console';
-    cd 'target/swift_navigation_console';
-    ls -l;
-    7z a -tzip "../../$OS_NAME.zip" *;
-    cd ../..;
-    echo "${OS_NAME}.zip" >bench.filename;
-    ls -l;
+    cp -r console_backend/tests/data "target/swift_navigation_console"
+    cp utils/bench_runner.py "target/swift_navigation_console"
+    cd "target/swift_navigation_console"
+    7z a -tzip "../../$OS_NAME.zip" *
+    cd ../..
+    echo "${OS_NAME}.zip" >bench.filename
+
+    ls -l
 fi

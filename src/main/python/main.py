1e1aa130180abaa96a1c13b9bab35b61be6875c0
Keith Kyzivat <keith.kyzivat@qt.io>
2021-10-07 16:55:54 -0400

    Quit if the QQmlApplicationEngine created no objects
    * This terminates the application abnormally if the loading of the base
      view.qml failed to create any objects. This typically happens if there
      is a syntax error or if a module couldn't be imported.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -731,12 +731,18 @@ if __name__ == "__main__":
     qmlRegisterType(UpdateTabData, "SwiftConsole", 1, 0, "UpdateTabData")  # type: ignore
 
     engine = QtQml.QQmlApplicationEngine()
+    qml_object_created = False
+    def handle_qml_load_errors(obj, url):  # pylint: disable=unused-argument
+        global qml_object_created  # pylint: disable=global-statement
+        qml_object_created = obj is not None
+    engine.objectCreated.connect(handle_qml_load_errors)
 
     capnp_path = get_capnp_path()
 
     engine.addImportPath("PySide2")
-
     engine.load(QUrl("qrc:/view.qml"))
+    if not qml_object_created:
+        sys.exit(1)
 
     messages_main = capnp.load(capnp_path)  # pylint: disable=no-member
 

0fe435a902608a33b309eba7d13b97d5433bbfc9
Steve Meyer <steve.meyer@swift-nav.com>
2021-10-08 14:32:17 -0700

    try entrypoint bin

---
+++ entrypoint/src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::os::unix::prelude::CommandExt;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[cfg(target_os = "windows")]
+fn find_py(dir: &Path) -> PathBuf {
+    dir.join("pythonw.exe")
+}
+
+#[cfg(not(target_os = "windows"))]
+fn find_py(dir: &Path) -> PathBuf {
+    let py3 = dir.join("python3");
+    if py3.exists() {
+        py3
+    } else {
+        dir.join("python")
+    }
+}
+
+fn main() -> Result<()> {
+    let me = env::current_exe()?;
+    let parent = me.parent().ok_or("no parent directory")?;
+    let py = find_py(parent);
+    let err = Command::new(py)
+        .arg("-m")
+        .arg("swiftnav_console.main")
+        .arg("--")
+        .args(env::args().skip(1))
+        .exec();
+    Err(err.into())
+}

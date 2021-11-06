478606419f6e9d2f64de09c85a21cf4189e5bd75
Jason Mobarak <jason@swiftnav.com>
2021-11-06 03:21:21 -0700

    remove lld

--- entrypoint/build.rs
+++ entrypoint/build.rs
@@ -2,11 +2,6 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         println!("cargo:rustc-link-lib=dylib=crypt");
-        println!("cargo:rustc-link-arg=-fuse-ld=lld-13");
         println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/lib");
-    }
-    #[cfg(target_os = "macos")]
-    {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/lib");
     }
 }

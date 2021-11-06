33ffd7c9edb0c2c9213caabff2ea416959db8001
Jason Mobarak <jason@swiftnav.com>
2021-11-06 02:57:12 -0700

    lld-13

--- entrypoint/build.rs
+++ entrypoint/build.rs
@@ -2,7 +2,7 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         println!("cargo:rustc-link-lib=dylib=crypt");
-        println!("cargo:rustc-link-arg=-fuse-ld=lld");
+        println!("cargo:rustc-link-arg=-fuse-ld=lld-13");
         println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/lib");
     }
     #[cfg(target_os = "macos")]

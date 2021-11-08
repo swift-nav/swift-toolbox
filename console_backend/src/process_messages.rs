b261aab87b41523f2228b3ce52daaa8243e14884
John Michael Burke <johnmichael.burke@swift-nav.com>
2021-11-08 10:36:29 -0800

    Relax flaky realtime delay tests[CPP-408]

--- console_backend/src/process_messages.rs
+++ console_backend/src/process_messages.rs
@@ -528,7 +528,7 @@ mod messages {
         use super::*;
 
         // wiggle room for timing the delay
-        const JIFFY: Duration = Duration::from_millis(1);
+        const JIFFY: Duration = Duration::from_millis(2);
 
         fn msg_gps_time(tow: u32) -> Sbp {
             MsgGpsTime {

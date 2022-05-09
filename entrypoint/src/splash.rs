b7556e20d7d732ebf1027105bb3d7346b6ecf3ca
Jason Mobarak <jason@swiftnav.com>
2022-05-09 15:20:48 -0700

    make fallible so we can remove the splash pid

--- entrypoint/src/splash.rs
+++ entrypoint/src/splash.rs
@@ -6,52 +6,56 @@ use std::{
     time::{Duration, Instant},
 };
 
+use lazy_static::lazy_static;
 use minifb::{Window, WindowOptions};
-
 use winit::{event_loop::EventLoop, window::Window as WinitWindow};
 
 use entrypoint::attach_console;
 
-
 const TIMEOUT_DURATION: Duration = Duration::from_secs(15);
 const TEMP_FILENAME: &str = "swiftnav_console";
 
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+lazy_static! {
+    static ref PID_FILE: PathBuf = {
+        let pid = std::process::id();
+        std::env::temp_dir().join(format!("{TEMP_FILENAME}.{pid}")).into()
+    };
+}
 
 fn rgb8_3_to_rgb32(r: u8, g: u8, b: u8) -> u32 {
     ((r as u32) << 16) | ((g as u32) << 8) | (b as u32)
 }
 
 fn create_temp_file() -> Result<PathBuf> {
-    let pid = std::process::id();
-    let temp_filename = std::env::temp_dir().join(format!("{TEMP_FILENAME}.{pid}"));
-    std::fs::File::create(&temp_filename)?;
-    Ok(temp_filename)
+    std::fs::File::create(&*PID_FILE)?;
+    Ok(PID_FILE.clone())
 }
 
-fn main() {
+fn launch_splash() -> Result<()> {
     attach_console();
     let logo = include_bytes!("../../resources/images/splash.jpg");
     let image = image::io::Reader::with_format(
         std::io::BufReader::new(Cursor::new(logo)),
         image::ImageFormat::Jpeg,
     )
-    .decode()
-    .unwrap();
+    .decode()?;
     let u32_buffer: Vec<u32> = image
         .as_bytes()
         .chunks(3)
         .map(|v| rgb8_3_to_rgb32(v[0], v[1], v[2]))
         .collect();
-    let current_monitor = WinitWindow::new(&EventLoop::new())
-        .unwrap()
+    let current_monitor = WinitWindow::new(&EventLoop::new())?
         .current_monitor()
-        .unwrap();
+        .ok_or_else(||{
+            Into::<Error>::into(String::from("could not get current monitor"))
+        })?;
     let size = current_monitor.size();
-    let pos_x = ((size.width as f64 / current_monitor.scale_factor() - image.width() as f64) / 2.0)
+    let pos_x = ((size.width as f64 - image.width() as f64) / 2.0)
         as isize;
-    let pos_y = ((size.height as f64 / current_monitor.scale_factor() - image.height() as f64)
+    let pos_y = ((size.height as f64 - image.height() as f64)
         / 2.0) as isize;
 
     let mut window = Window::new(
@@ -65,15 +69,25 @@ fn main() {
             none: true,
             ..WindowOptions::default()
         },
-    )
-    .expect("unable to open window");
+    )?;
 
-    let temp_filename = create_temp_file().unwrap();
+    let temp_filename = create_temp_file()?;
     let now = Instant::now();
     while window.is_open() && now.elapsed() < TIMEOUT_DURATION && temp_filename.exists() {
         window
-            .update_with_buffer(&u32_buffer, image.width() as usize, image.height() as usize)
-            .unwrap();
+            .update_with_buffer(&u32_buffer, image.width() as usize, image.height() as usize)?;
         window.set_position(pos_x, pos_y);
     }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let result = launch_splash();
+    if let Err(ref err) = result {
+        eprint!("Error launching splash screen: {err}");
+    }
+    // Try to remove the file, don't care about the result
+    let _result = std::fs::remove_file(&*PID_FILE);
+    result
 }

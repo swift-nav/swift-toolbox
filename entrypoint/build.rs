b68d665d931631e59dfb6943dc3b179f38c6c6ae
Jason Mobarak <jason@swiftnav.com>
2022-05-31 15:36:55 -0700

    Fix-up formatting

--- entrypoint/build.rs
+++ entrypoint/build.rs
@@ -3,7 +3,6 @@ use winres::WindowsResource;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
-
     #[cfg(target_os = "linux")]
     {
         println!("cargo:rustc-link-lib=dylib=crypt");

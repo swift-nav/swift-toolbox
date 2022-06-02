dee416eb4991eb4933ff9dd87d4585f88e61863f
Jason Mobarak <jason@swiftnav.com>
2022-06-02 12:59:36 -0700

    splash screen with a version [CPP-767] (#580)
    Jira: https://swift-nav.atlassian.net/browse/CPP-767
    
    Adds a version number to the splash screen per request from apps.
    
    Uses ImageMagick to automate. Customer installation of ImageMagick implemented here: https://github.com/swift-nav/swift-toolchains/tree/master/imagemagick

--- entrypoint/build.rs
+++ entrypoint/build.rs
@@ -8,13 +8,21 @@ fn main() -> Result<()> {
         println!("cargo:rustc-link-lib=dylib=crypt");
         println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/lib");
     }
+
     #[cfg(target_os = "windows")]
     {
         WindowsResource::new()
             .set_icon("../resources/images/icon.ico")
             .compile()?;
+    }
 
+    if std::fs::metadata("../resources/images/splash-version.jpg").is_ok() {
+        println!("cargo:rustc-env=CONSOLE_SPLASH_IMAGE=resources/images/splash-version.jpg");
+        println!("cargo:rerun-if-changed=../resources/images/splash-version.jpg");
+    } else {
+        println!("cargo:rustc-env=CONSOLE_SPLASH_IMAGE=resources/images/splash.jpg");
         println!("cargo:rerun-if-changed=../resources/images/splash.jpg");
     }
+
     Ok(())
 }

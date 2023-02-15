c46ba69700d7c32dc5ef5605bdc3dd699b6142e6
adrian-kong <adrian.k1234567890@gmail.com>
2023-02-15 17:24:00 +1100

    add custom plugins + controls

--- resources/web/map/js/trajectory.js
+++ resources/web/map/js/trajectory.js
@@ -1,35 +1,45 @@
-const style = {
-    "version": 8,
-    "sources": {
-        "osm": {
-            "type": "raster",
-            // "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
-            "tiles": ["https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3dpZnQtYWRyaWFuIiwiYSI6ImNsZTN1MW82bDA2OGgzdXFvOWFuZTJlMnEifQ.9nR8m0C-B_ISNR4r4cMExw"],
-            "tileSize": 256,
-            "attribution": "&copy; OpenStreetMap Contributors",
-            "maxzoom": 22
-        }
-    },
-    "layers": [
-        {
-            "id": "osm",
-            "type": "raster",
-            "source": "osm"
-        }
-    ]
-};
+import mapboxGlStyleSwitcher from 'https://cdn.skypack.dev/mapbox-gl-style-switcher';
 
-var map = new maplibregl.Map({
+mapboxgl.accessToken = "pk.eyJ1Ijoic3dpZnQtYWRyaWFuIiwiYSI6ImNsZTN1MW82bDA2OGgzdXFvOWFuZTJlMnEifQ.9nR8m0C-B_ISNR4r4cMExw";
+var map = new mapboxgl.Map({
     container: 'map',
-    style,
+    style: "mapbox://styles/mapbox/dark-v11",
     center: [-122.486052, 37.830348],  // Initial focus coordinate
     zoom: 16
 });
 
-// MapLibre GL JS does not handle RTL text by default, so we recommend adding this dependency to fully support RTL rendering.
-maplibregl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.1/mapbox-gl-rtl-text.js');
-// Add zoom and rotation controls to the map.
-map.addControl(new maplibregl.NavigationControl());
+var focus = false;
+
+class FocusToggle {
+    onAdd(map) {
+        this._map = map;
+        this._btn = document.createElement("button");
+        this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-focus-toggle";
+        this._btn.type = "button";
+        this._btn.onclick = () => {
+            console.log(this._btn.className);
+            focus = !focus;
+            if (focus) {
+                this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-unfocus-toggle";
+            } else {
+                this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-focus-toggle";
+            }
+        };
+        this._container = document.createElement("div");
+        this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+        this._container.appendChild(this._btn);
+        return this._container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+}
+
+map.addControl(new mapboxGlStyleSwitcher.MapboxStyleSwitcherControl());
+map.addControl(new FocusToggle(), "top-right");
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function () {
     console.log("loaded");
@@ -67,10 +77,12 @@ map.on('load', function () {
             console.log(`received ${id} ${lat} ${lng}`);
             const pos = [lat, lng];
             if (!start) {
-                new maplibregl.Marker().setLngLat(pos).addTo(map);
+                new mapboxgl.Marker().setLngLat(pos).addTo(map);
                 start = pos;
+                map.panTo(pos);
+            } else if (focus) {
+                map.panTo(pos);
             }
-            map.panTo(pos);
             data.geometry.coordinates.push(pos);
             map.getSource('route').setData(data);
         })

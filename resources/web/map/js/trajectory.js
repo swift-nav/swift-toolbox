7df4d2221552ab041c62a339a40a933610daa181
adrian-kong <adrian.k1234567890@gmail.com>
2023-02-15 13:11:44 +1100

    using mapbox satellite tiles

--- resources/web/map/js/trajectory.js
+++ resources/web/map/js/trajectory.js
@@ -1,25 +1,13 @@
-var data = {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-        type: 'LineString',
-        coordinates: []
-    }
-};
-
 const style = {
     "version": 8,
     "sources": {
         "osm": {
             "type": "raster",
-            "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            // "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tiles": ["https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3dpZnQtYWRyaWFuIiwiYSI6ImNsZTN1MW82bDA2OGgzdXFvOWFuZTJlMnEifQ.9nR8m0C-B_ISNR4r4cMExw"],
             "tileSize": 256,
             "attribution": "&copy; OpenStreetMap Contributors",
             "maxzoom": 22
-        },
-        "route": {
-            "type": "geojson",
-            data
         }
     },
     "layers": [
@@ -27,15 +15,6 @@ const style = {
             "id": "osm",
             "type": "raster",
             "source": "osm"
-        },
-        {
-            "id": "route",
-            "type": "circle",
-            "source": "route",
-            "paint": {
-                'circle-color': 'red',
-                'circle-radius': 3,
-            }
         }
     ]
 };
@@ -49,13 +28,40 @@ var map = new maplibregl.Map({
 
 // MapLibre GL JS does not handle RTL text by default, so we recommend adding this dependency to fully support RTL rendering.
 maplibregl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.1/mapbox-gl-rtl-text.js');
-
 // Add zoom and rotation controls to the map.
 map.addControl(new maplibregl.NavigationControl());
 
 map.on('load', function () {
     console.log("loaded");
     var start;
+
+    var data = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'LineString',
+            coordinates: []
+        }
+    };
+
+    const source = {
+        type: 'geojson',
+        cluster: false,
+        data
+    };
+
+    map.addSource('route', source)
+
+    map.addLayer({
+        "id": "route",
+        "type": "circle",
+        "source": "route",
+        "paint": {
+            'circle-color': 'red',
+            'circle-radius': 3,
+        }
+    });
+
     new QWebChannel(qt.webChannelTransport, (channel) => {
         channel.objects.currPos.recvPos.connect((id, lat, lng) => {
             console.log(`received ${id} ${lat} ${lng}`);

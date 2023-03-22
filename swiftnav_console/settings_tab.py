91bb6afb07dfa83b5ca47451acb5478f64d1d27f
adrian-kong <adrian.k1234567890@gmail.com>
2023-03-22 17:19:47 +1100

    lint

--- swiftnav_console/settings_tab.py
+++ swiftnav_console/settings_tab.py
@@ -86,7 +86,7 @@ class SettingsTabData(QObject):
         self.settings_import_status = SETTINGS_IMPORT_STATUS[0]
         self.settings_ins = SETTINGS_INS[0]
         self.settings_notification = SETTINGS_NOTIFICATION[0]
-        self.update()
+        self.update()  # type: ignore
 
     def get_import_status(self) -> str:
         return self._import_status

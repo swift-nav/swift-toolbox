03c7a1329c393dd3f28b9c1bd8f4fbd9f07a9297
adrian-kong <35755741+adrian-kong@users.noreply.github.com>
2023-05-23 12:06:47 +1000

    Revert "title-dash only when connected (#1056)"
    This reverts commit cae0e7f0605b34f812dd9f321529634e183c9fdd.

--- console_backend/src/status_bar.rs
+++ console_backend/src/status_bar.rs
@@ -182,12 +182,10 @@ impl StatusBar {
         status_bar_status.set_ins(&sb_update.ins_status);
         status_bar_status.set_data_rate(sb_update.data_rate);
         status_bar_status.set_solid_connection(sb_update.solid_connection);
-        let port = if sb_update.port.is_empty() {
-            "".to_string()
-        } else {
-            sb_update.port + " - "
-        };
-        status_bar_status.set_title(&format!("{port}Swift Console {}", sb_update.version));
+        status_bar_status.set_title(&format!(
+            "{} - Swift Console {}",
+            sb_update.port, sb_update.version
+        ));
         client_sender.send_data(serialize_capnproto_builder(builder));
     }
 

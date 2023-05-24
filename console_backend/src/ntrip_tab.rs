8b2eb4912f043dddffbd58a624a389b06b777db9
adrian-kong <adrian.k1234567890@gmail.com>
2023-05-24 10:33:45 +1000

    error to logg panel

--- console_backend/src/ntrip_tab.rs
+++ console_backend/src/ntrip_tab.rs
@@ -15,6 +15,7 @@ use crate::status_bar::Heartbeat;
 
 use anyhow::Context;
 
+use log::error;
 use std::time::{Duration, SystemTime};
 
 #[derive(Debug, Default)]
@@ -207,7 +208,7 @@ fn main(
             heartbeat.set_ntrip_ul(_ulnow);
             if !rx.is_empty() {
                 if let Err(e) = transfer.borrow().unpause_read() {
-                    println!("unpause error: {e}");
+                    error!("ntrip unpause error: {e}");
                     return false;
                 }
             }
@@ -218,7 +219,7 @@ fn main(
     transfer.borrow_mut().write_function(|data| {
         std::io::stdout().write_all(data).unwrap(); // should be infallible
         if let Err(e) = msg_sender.write_all(data) {
-            println!("write error: {e}");
+            error!("ntrip write error: {e}");
             return Ok(0);
         }
         Ok(data.len())
@@ -230,7 +231,7 @@ fn main(
         };
         bytes.extend_from_slice(b"\r\n");
         if let Err(e) = data.write_all(&bytes) {
-            println!("read error: {e}");
+            error!("ntrip read error: {e}");
             return Err(ReadError::Abort);
         }
         Ok(bytes.len())
